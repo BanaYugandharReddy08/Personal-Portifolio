d5edc81898adb84156b12591670844ecb09ace84
BanaYugandharReddy08 <157593298+BanaYugandharReddy08@users.noreply.github.com>
2025-06-28 21:26:47 +0100

    Updating the router

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
-import App from './App'
-import './index.css'
-import { AuthProvider } from './context/AuthContext.js'
-import { ThemeProvider } from './context/ThemeContext.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter as Router } from 'react-router-dom'; // ← change here
+import App from './App';
+import './index.css';
+import { AuthProvider } from './context/AuthContext.js';
+import { ThemeProvider } from './context/ThemeContext.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
       <AuthProvider>
-        <Router>
+        <Router> {/* Now using HashRouter */}
           <App />
         </Router>
       </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
-)
+);
