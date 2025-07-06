55cfb865fdb4864d83a10b039cf3df7a4225ddbd
Bana Yugandhar Reddy <157593298+BanaYugandharReddy08@users.noreply.github.com>
2025-07-06 19:36:03 +0100

    Add dashboard tabs and components

---
+++ src/components/dashboard/DashboardProjects.js
@@ -0,0 +1,9 @@
+const DashboardProjects = () => {
+  return (
+    <div className="dashboard-placeholder">
+      <h2>Project management coming soon.</h2>
+    </div>
+  );
+};
+
+export default DashboardProjects;
